7dee479772013e160935836e399ac9f2f02cf240
Yulia Bystrovskya <julikatysheva@gmail.com>
2025-05-08 11:51:14 +0300

    final

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -96,10 +96,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/static'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
